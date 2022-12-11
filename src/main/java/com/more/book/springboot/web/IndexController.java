d1deb823a67672bc5167bd66648bb87af6d61a95
won-mo1 <booboo929292@gmail.com>
2022-12-11 20:47:42 +0900

    20221121

--- src/main/java/com/more/book/springboot/web/IndexController.java
+++ src/main/java/com/more/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.more.book.springboot.web;
 
+import com.more.book.springboot.config.auth.LoginUser;
+import com.more.book.springboot.config.auth.dto.SessionUser;
 import com.more.book.springboot.service.posts.PostsService;
 import com.more.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,23 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
         //mustache 스타터 덕분에 컨트롤러에서 문자열을 반화할 때 앞의경로와 뒤의 파일확장자는 자동으로 지정된다.
         //앞의 경로는 src/main/resources/templates/index.mustache 로 전환되어 View Resolver 가 처리하게 됨
